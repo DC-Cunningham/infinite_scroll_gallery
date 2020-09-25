7dda5ffda4402584dbeb51eae82fcabe3b0d531f
DCRevResLabs <dc@revolutionresearch.com.au>
2020-09-25 14:53:00 +1000

    optimised photo count for load

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // API Setup
-const count = 10;
+const count = 4;
 const apiKey = "PpfXjPmTosr2-DHANp4unoqYk1FPT9Ifpd7XTAltq7I";
-const collection = "architecture";
+const collection = "Architecture";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&collections=${collection}&count=${count}`;
+
+// Set attribute helper
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Check if all images have benn loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    count = 10;
+  }
+}
 
 // create elements for links, photos and add to document
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
   photosArray.forEach((photo) => {
     // create <a> to link to unsplash photo
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    setAttributes(item, { href: photo.links.html, target: "_blank" });
     // Create image
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // Event Listener to check when each image is finished loading
+    img.addEventListener("load", imageLoaded);
     // put image inside document
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -37,6 +62,18 @@ async function getPhotos() {
     // Catch error
   }
 }
+
+// Event Listeners
+// Get more photos on scroll to bottom of page
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // On Load
 getPhotos();
